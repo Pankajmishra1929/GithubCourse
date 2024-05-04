6aecb294f06cb0753ee3dcc727cee1a842e802ee
Pankaj Mishra <86742019+Pankajmishra1929@users.noreply.github.com>
2024-05-04 15:50:26 +0530

    Create ClassicPage.java

---
+++ src/pages/ClassicPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+public class ClassicPage {
+
+	public void classic() {
+		// TODO Auto-generated method stub
+     
+		 int k=100;
+		 int k1=200;
+		 int div= k1/k;
+		 System.out.println(div);
+     System.out.println("bye");
+	}
+
+}
